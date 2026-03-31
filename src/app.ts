1b8b18a1ac2557dd90e7093af7a0e09c550fc4fe
Andrei Anoling <anoling.andrei@gmail.com>
2026-03-31 16:20:33 -0500

    Feat: Added openapi documentation for all endpoints in postRoutes.ts

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import morgan from "morgan";
 import { helmetConfig } from "../config/helmetConfig";
 import cors from "cors";
 import { corsOptions } from "../config/corsConfig";
+import setupSwagger from "../config/swagger";
 
 
 const app = express();
@@ -15,6 +16,8 @@ app.use(cors(corsOptions()));
 app.use(helmetConfig());
 
 app.use(express.json());
+
+setupSwagger(app)
 
 app.use("/api/v1/events", router);
 
